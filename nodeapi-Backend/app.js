b0b8386ad5ab9730732bed44a70b8c481ff0a5fb
Shivam Singh <48352226+SHIVAMSINGH0101@users.noreply.github.com>
2020-02-08 09:31:57 +0530

    Add files via upload

--- nodeapi-Backend/app.js
+++ nodeapi-Backend/app.js
@@ -11,24 +11,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-/*
-// db
-// mongodb://kaloraat:dhungel8@ds257054.mlab.com:57054/nodeapi
-// MONGO_URI=mongodb://localhost/nodeapi
-// mongodb+srv://kaloraat_admin:kkkkkk9@nodeapi-pbn7j.mongodb.net/nodeapi?retryWrites=truenodeAPI?retryWrites=true
-// mongodb+srv://robertchou_admin:Aeiourc2491@nodeapi-p2o93.mongodb.net/nodeapi?retryWrites=true&w=majority
-mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true
-    })
-    .then(() => console.log('DB Connected'));
-
-mongoose.connection.on('error', err => {
-    console.log(`DB connection error: ${err.message}`);
-});
-*/
-
-
 //connecting to db
 //connect to mongoose Server
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true,  useUnifiedTopology: true }).then(()=> console.log("DB connected"));
@@ -36,25 +18,6 @@ mongoose.connection.on("error", err=>{
   console.log("DB connection error:",err);
 });
 
-
-/*
-// bring in routes
-const postRoutes = require('./routes/post');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-// apiDocs
-app.get('/api', (req, res) => {
-    fs.readFile('docs/apiDocs.json', (err, data) => {
-        if (err) {
-            res.status(400).json({
-                error: err
-            });
-        }
-        const docs = JSON.parse(data);
-        res.json(docs);
-    });
-});
-*/
 
 
 //Bring in routes from routes folder which acts as a middleware
@@ -76,28 +39,6 @@ app.get("/", (req, res) => {
 });
 
 
-/*
-// middleware -
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(expressValidator());
-app.use(cors());
-app.use('/api', postRoutes);
-app.use('/api', authRoutes);
-app.use('/api', userRoutes);
-app.use(function(err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).json({ error: 'Unauthorized!' });
-    }
-});
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log(`A Node Js API is listening on port: ${port}`);
-});
-*/
-
 
 //middleware
 //not using .get because we are using as middleware.
@@ -115,10 +56,6 @@ app.use(function (err, req, res, next) {
   }
 });
 
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//     console.log(`A Node Js API is listening on port: ${port}`);
-// });
 
 const port=process.env.PORT || 8080;
 app.listen(port, ()=>{
